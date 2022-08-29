5e9f458f1c14b8c3669d58ce4dab4a24cdfbd34e
dwiriad <99604497+dwiriad@users.noreply.github.com>
2022-08-29 11:43:14 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -33,15 +33,18 @@ Route::get('/blog', function(){
     $blog_posts = [
         [
             "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
             "author" => "Dwi Ria Wulandari",
             "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum voluptatibus vel officia ea sunt dolor. Amet esse exercitationem quia, quis doloremque sunt deserunt ut, incidunt temporibus autem cum quaerat illum ea rem nostrum aut odit! Voluptate quasi, vitae perferendis corrupti tempora, dolorum perspiciatis doloribus cum itaque nobis fugit quas consectetur, iure impedit voluptatem! Veritatis omnis aut soluta fugit similique. Iste temporibus nemo aspernatur molestiae laudantium. Aliquid, doloremque incidunt delectus perspiciatis obcaecati deserunt aperiam nulla cum. Quaerat iusto modi quae in."
         ],
         [
             "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
             "author" => "Adisti Khairunnisa",
             "body" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur cum eos omnis corporis vero ipsam ullam iure quidem animi fugiat, dolorem doloremque nisi praesentium quaerat quisquam aut soluta eum! Nostrum illo eum quia consequuntur accusamus, tempore quod reprehenderit voluptates suscipit delectus similique sed, fugit minus nulla sequi itaque quisquam aliquam beatae iure. Atque tempore quae ad necessitatibus sit inventore consequatur, vero voluptate. Eligendi, saepe a, pariatur illo dolore impedit dolores architecto quo sed eaque labore laudantium? Quisquam quod libero unde omnis nisi. Id enim voluptatem a veritatis laboriosam fugit maiores rem, voluptatibus incidunt vel, aliquid soluta aperiam porro. Cupiditate, laboriosam!"
         ],
     ];
+
     return view('posts', [
         "title" => "Posts",
         "posts" => $blog_posts
